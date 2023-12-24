ba2203f4a1ae9ab8b35bbe71345fa89ffcd1d1a3
Leandro <devleandrodosanjos@gmail.com>
2023-12-23 22:27:08 -0300

    Programa para calcular a média

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String args[]){
+
+        Double media = calculo.calculoNota();
+        System.out.println("*** A média das notas é: " + media);
+
+    }
+
+}
---
+++ src/calculo.java
@@ -0,0 +1,16 @@
+public class calculo {
+
+
+    private double media;
+    public static Double calculoNota() {
+
+         double nota1 = 7.5;
+         double nota2 = 8.3;
+         double nota3 = 4.7;
+         double nota4 = 5.2;
+
+        double mediaC = (nota1 + nota2 + nota3 + nota4)/4;
+        return mediaC;
+    }
+
+}
